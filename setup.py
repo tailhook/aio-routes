677b8ce335ce812b63774df3e9c0cbfb8a46c685
Paul Colomiets <paul@colomiets.name>
2014-07-10 18:24:49 +0300

    Version bumped to v0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='aio-routes',
-      version='0.1',
+      version='0.2',
       description='Routing for asyncio',
       author='Paul Colomiets',
       author_email='paul@colomiets.name',
